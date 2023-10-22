000f79a226ff84b20cfe3751a25e9e8c17c43fdf
Jonatas Freitas <103526432+xBigJhow@users.noreply.github.com>
2023-10-21 22:31:09 -0300

    Delete src directory

--- src/factoryMethod/fabricas/Fabrica.java
+++
@@ -1,5 +0,0 @@
-package factoryMethod.fabricas;
-
-public interface Fabrica {
-	public void fazerPizza(String sabor);
-}
--- src/factoryMethod/pizzas/Pizza.java
+++
@@ -1,8 +0,0 @@
-package factoryMethod.pizzas;
-
-public interface Pizza {
-	public void preparar();
-	public void assar();
-	public void cortar();
-	public void embalar();
-}
